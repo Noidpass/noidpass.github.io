aaa773aac99bb8a3b2390a5d7e67985aba71a1b2
noidpass <didi.kumar2@gmail.com>
2022-03-15 15:13:19 +0700

    first commit

---
+++ inden/js/coursel1.js
@@ -0,0 +1,9 @@
+
+$("#stage").owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 100,
+    items: 3,
+    nav: true,
+    loop: true,
+});
---
+++ inden/js/coursel2.js
@@ -0,0 +1,10 @@
+$("#hero").owlCarousel({
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 100,
+    items: 1,
+    nav: false,
+    loop: true,
+    animateIn: 'flipInY',
+    animateOut: 'zoomOutDown'
+});
